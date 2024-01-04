bb93da0c5b37b8bdb3a5185ea82660e22fa73294
Luis Carlos Pando <hey@luiscarlospando.com>
2024-01-04 12:00:23 -0600

    Actualizar footer

--- footer.php
+++ footer.php
@@ -1,34 +1,150 @@
-			<!-- footer -->
-			<footer class="footer" role="contentinfo">
+	<footer>
+        <div class="container">
+            <div class="row mb-3">
+                <div class="col-lg-6">
+                    <div class="row mb-4">
+                        <div class="col-6">
+                            <nav>
+                                <ul class="list-unstyled">
+                                    <li>
+                                        Our Solutions
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            e-Commerce
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            Social Media
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            App Development
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            Customer Experience
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            Content Management
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            SEO/SEM
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            Automation
+                                        </a>
+                                    </li>
+                                </ul>
+                            </nav>
+                        </div>
+                        <div class="col-6">
+                            <nav>
+                                <ul class="list-unstyled">
+                                    <li>
+                                        Portfolio
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            Project List
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            Project Details
+                                        </a>
+                                    </li>
+                                </ul>
+                            </nav>
+                        </div>
+                    </div>
+                    <div class="row mb-4">
+                        <div class="col-6">
+                            <nav>
+                                <ul class="list-unstyled">
+                                    <li>
+                                        Our opinion
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            Blog
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            The Team
+                                        </a>
+                                    </li>
+                                </ul>
+                            </nav>
+                        </div>
+                        <div class="col-6">
+                            <nav>
+                                <ul class="list-unstyled">
+                                    <li>
+                                        Platform
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            Mixen Cloud
+                                        </a>
+                                    </li>
+                                </ul>
+                            </nav>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-6">
+                            <nav>
+                                <ul class="list-unstyled">
+                                    <li class="mb-3">
+                                        <a href="#">
+                                            Privacy Policy
+                                        </a>
+                                    </li>
+                                    <li class="mb-3">
+                                        <a href="#">
+                                            Cookie Policy
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            Terms & Conditions
+                                        </a>
+                                    </li>
+                                </ul>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 my-lg-auto">
+                    <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/footer/thumb.jpg" alt="" class="img-fluid">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <p class="mb-0">&copy; <?php echo date('Y'); ?> <?php bloginfo( 'name' ); ?>. All rights reserved.</p>
+                </div>
+                <div class="col text-end">
+                    <p class="mb-0">Made with <i class="fa-solid fa-heart" alt="love" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="love"></i> @ Mixen</p>
+                </div>
+            </div>
+        </div>
+    </footer>
 
-				<!-- copyright -->
-			    <?php
-				    printf( '<p class="copyright">' . __( '&copy; %1$s Copyright %2$s. Powered by <a href="%3$s" title="WordPress">WordPress</a> &amp; <a href="%4$s" title="HTML5 Blank">HTML5 Blank</a>.', 'html5blank' ) . '</p>',
-				    	date( 'Y'),
-				    	esc_html( get_bloginfo( 'name' ) ),
-				    	'//wordpress.org',
-				    	'//html5blank.com'
-					);
-				?>
-				<!-- /copyright -->
+    <!-- a href="https://wa.me/52614?text=Hola%20FOO%20BAR,%20necesito%20información." class="whatsapp" target="_blank">
+        <i class="fab fa-whatsapp whatsapp-icon"></i>
+    </a -->
 
-			</footer>
-			<!-- /footer -->
-
-		</div>
-		<!-- /wrapper -->
-
-		<?php wp_footer(); ?>
-
-		<!-- analytics -->
-		<script>
-		(function(f,i,r,e,s,h,l){i['GoogleAnalyticsObject']=s;f[s]=f[s]||function(){
-		(f[s].q=f[s].q||[]).push(arguments)},f[s].l=1*new Date();h=i.createElement(r),
-		l=i.getElementsByTagName(r)[0];h.async=1;h.src=e;l.parentNode.insertBefore(h,l)
-		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-		ga('create', 'UA-XXXXXXXX-XX', 'yourdomain.com');
-		ga('send', 'pageview');
-		</script>
-
-	</body>
+    <script src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/js/app.bundle.js"></script>
+</body>
 </html>
