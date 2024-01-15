fb9da93cf858d23731b249c8b89cfc340fff20f2
Luis Carlos Pando <hey@luiscarlospando.com>
2024-01-15 11:07:15 -0600

    Agregar home-podcast

--- functions.php
+++ functions.php
@@ -29,6 +29,7 @@ if ( function_exists( 'add_theme_support' ) ) {
     add_image_size( 'medium', 250, '', true ); // Medium Thumbnail.
     add_image_size( 'small', 120, '', true ); // Small Thumbnail.
     add_image_size( 'home-blog', 781, 363, true ); // Custom Thumbnail Size call using the_post_thumbnail('home-blog');
+    add_image_size( 'home-podcast', 781, 325, true ); // Custom Thumbnail Size call using the_post_thumbnail('home-podcast');
     add_image_size( 'custom-size', 700, 200, true ); // Custom Thumbnail Size call using the_post_thumbnail('custom-size');
 
     // Add Support for Custom Backgrounds - Uncomment below if you're going to use.
