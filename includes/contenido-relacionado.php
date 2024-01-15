3aa0892fb1aeaf187a7db93d9a35610cf494831b
Luis Carlos Pando <hey@luiscarlospando.com>
2024-01-15 15:29:44 -0600

    Armar shingle

---
+++ includes/contenido-relacionado.php
@@ -0,0 +1,170 @@
+    <section id="portafolio" class="common-light py-60">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <h1>
+                        Contenido<br>
+                        relacionado
+                    </h1>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col overflow-hidden">
+                    <!-- Slider main container -->
+                    <div class="swiper-relacionados">
+                        <!-- Additional required wrapper -->
+                        <div class="swiper-wrapper">
+                            <!-- Inicio Slide -->
+                            <div class="swiper-slide">
+                                <div class="row">
+                                    <div class="col-12">
+                                        <a href="#">
+                                            <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/resultados/slide-1.png" alt="" class="img-fluid mb-3">
+                                        </a>
+                                        <div class="row">
+                                            <div class="col-10 my-auto">
+                                                <a href="#">
+                                                    <h2>Farmacias Guadalajara</h2>
+                                                </a>
+                                            </div>
+                                            <div class="col-2 my-auto text-end">
+                                                <a href="#">
+                                                    <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/ico-arrow.png" alt="" id="ico-arrow" class="img-fluid">
+                                                </a>
+                                            </div>
+                                        </div>
+                                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur deserunt corporis explicabo enim illo assumenda ullam cumque reprehenderit nisi modi labore maiores, debitis saepe cupiditate illum, earum et voluptate quisquam!</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- /Fin Slide -->
+                             <!-- Inicio Slide -->
+                             <div class="swiper-slide">
+                                <div class="row">
+                                    <div class="col-12">
+                                        <a href="#">
+                                            <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/resultados/slide-1.png" alt="" class="img-fluid mb-3">
+                                        </a>
+                                        <div class="row">
+                                            <div class="col-10 my-auto">
+                                                <a href="#">
+                                                    <h2>Farmacias Guadalajara</h2>
+                                                </a>
+                                            </div>
+                                            <div class="col-2 my-auto text-end">
+                                                <a href="#">
+                                                    <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/ico-arrow.png" alt="" id="ico-arrow" class="img-fluid">
+                                                </a>
+                                            </div>
+                                        </div>
+                                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur deserunt corporis explicabo enim illo assumenda ullam cumque reprehenderit nisi modi labore maiores, debitis saepe cupiditate illum, earum et voluptate quisquam!</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- /Fin Slide -->
+                             <!-- Inicio Slide -->
+                             <div class="swiper-slide">
+                                <div class="row">
+                                    <div class="col-12">
+                                        <a href="#">
+                                            <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/resultados/slide-1.png" alt="" class="img-fluid mb-3">
+                                        </a>
+                                        <div class="row">
+                                            <div class="col-10 my-auto">
+                                                <a href="#">
+                                                    <h2>Farmacias Guadalajara</h2>
+                                                </a>
+                                            </div>
+                                            <div class="col-2 my-auto text-end">
+                                                <a href="#">
+                                                    <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/ico-arrow.png" alt="" id="ico-arrow" class="img-fluid">
+                                                </a>
+                                            </div>
+                                        </div>
+                                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur deserunt corporis explicabo enim illo assumenda ullam cumque reprehenderit nisi modi labore maiores, debitis saepe cupiditate illum, earum et voluptate quisquam!</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- /Fin Slide -->
+                             <!-- Inicio Slide -->
+                             <div class="swiper-slide">
+                                <div class="row">
+                                    <div class="col-12">
+                                        <a href="#">
+                                            <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/resultados/slide-1.png" alt="" class="img-fluid mb-3">
+                                        </a>
+                                        <div class="row">
+                                            <div class="col-10 my-auto">
+                                                <a href="#">
+                                                    <h2>Farmacias Guadalajara</h2>
+                                                </a>
+                                            </div>
+                                            <div class="col-2 my-auto text-end">
+                                                <a href="#">
+                                                    <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/ico-arrow.png" alt="" id="ico-arrow" class="img-fluid">
+                                                </a>
+                                            </div>
+                                        </div>
+                                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur deserunt corporis explicabo enim illo assumenda ullam cumque reprehenderit nisi modi labore maiores, debitis saepe cupiditate illum, earum et voluptate quisquam!</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- /Fin Slide -->
+                             <!-- Inicio Slide -->
+                             <div class="swiper-slide">
+                                <div class="row">
+                                    <div class="col-12">
+                                        <a href="#">
+                                            <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/resultados/slide-1.png" alt="" class="img-fluid mb-3">
+                                        </a>
+                                        <div class="row">
+                                            <div class="col-10 my-auto">
+                                                <a href="#">
+                                                    <h2>Farmacias Guadalajara</h2>
+                                                </a>
+                                            </div>
+                                            <div class="col-2 my-auto text-end">
+                                                <a href="#">
+                                                    <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/ico-arrow.png" alt="" id="ico-arrow" class="img-fluid">
+                                                </a>
+                                            </div>
+                                        </div>
+                                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur deserunt corporis explicabo enim illo assumenda ullam cumque reprehenderit nisi modi labore maiores, debitis saepe cupiditate illum, earum et voluptate quisquam!</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- /Fin Slide -->
+                             <!-- Inicio Slide -->
+                             <div class="swiper-slide">
+                                <div class="row">
+                                    <div class="col-12">
+                                        <a href="#">
+                                            <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/resultados/slide-1.png" alt="" class="img-fluid mb-3">
+                                        </a>
+                                        <div class="row">
+                                            <div class="col-10 my-auto">
+                                                <a href="#">
+                                                    <h2>Farmacias Guadalajara</h2>
+                                                </a>
+                                            </div>
+                                            <div class="col-2 my-auto text-end">
+                                                <a href="#">
+                                                    <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/ico-arrow.png" alt="" id="ico-arrow" class="img-fluid">
+                                                </a>
+                                            </div>
+                                        </div>
+                                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur deserunt corporis explicabo enim illo assumenda ullam cumque reprehenderit nisi modi labore maiores, debitis saepe cupiditate illum, earum et voluptate quisquam!</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- /Fin Slide -->
+                        </div>
+
+                        <!-- If we need navigation buttons -->
+                        <div class="swiper-button-prev"></div>
+                        <div class="swiper-button-next"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
--- single.php
+++ single.php
@@ -1,71 +1,50 @@
 <?php get_header(); ?>
 
-	<main role="main" aria-label="Content">
-	<!-- section -->
-	<section>
+	<section id="jumbotron" class="parallax-window" data-parallax="scroll" data-image-src="<?php the_post_thumbnail('large', array('class' => 'img-fluid')); ?>"></section>
 
-	<?php if ( have_posts() ) : while (have_posts() ) : the_post(); ?>
+    <section id="body" class="py-60">
+        <div class="container">
+            <div class="row mb-1 mb-lg-3">
+                <div class="col">
+					<h1 class="titulo">
+                        <span class="fs-4"><?php the_title(); ?></span>
+                    </h1>
+                    <p class="subtitulo">
+                        <small>
+							<ul class="list-inline">
+								<li class="list-inline-item">
+								<?php
+									// Get the author's name
+									$author_name = get_the_author();
 
-		<!-- article -->
-		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+									// Get the post date
+									$post_date = get_the_date('j \d\e F, Y');
 
-			<!-- post thumbnail -->
-			<?php if ( has_post_thumbnail() ) : // Check if Thumbnail exists. ?>
-				<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
-					<?php the_post_thumbnail(); // Fullsize image for the single post. ?>
-				</a>
-			<?php endif; ?>
-			<!-- /post thumbnail -->
+									// Get the published time
+									$published_time = get_the_time('H:i');
 
-			<!-- post title -->
-			<h1>
-				<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a>
-			</h1>
-			<!-- /post title -->
+									// Output the post data
+									echo 'Publicado por ' . $author_name . ' el ' . $post_date . ' a la(s) ' . $published_time;
+								?>
+								</li>
+								<li class="list-inline-item">
+									<?php edit_post_link(); ?>
+								</li>
+							</ul>
+						</small>
+                    </p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+					<?php the_content(); ?>
+                </div>
+            </div>
+        </div>
+    </section>
 
-			<!-- post details -->
-			<span class="date">
-				<time datetime="<?php the_time( 'Y-m-d' ); ?> <?php the_time( 'H:i' ); ?>">
-					<?php the_date(); ?> <?php the_time(); ?>
-				</time>
-			</span>
-			<span class="author"><?php esc_html_e( 'Published by', 'html5blank' ); ?> <?php the_author_posts_link(); ?></span>
-			<span class="comments"><?php if ( comments_open( get_the_ID() ) ) comments_popup_link( __( 'Leave your thoughts', 'html5blank' ), __( '1 Comment', 'html5blank' ), __( '% Comments', 'html5blank' ) ); ?></span>
-			<!-- /post details -->
+    <?php get_template_part( 'includes/contenido-relacionado' ); ?>
 
-			<?php the_content(); // Dynamic Content. ?>
-
-			<?php the_tags( __( 'Tags: ', 'html5blank' ), ', ', '<br>' ); // Separated by commas with a line break at the end. ?>
-
-			<p><?php esc_html_e( 'Categorised in: ', 'html5blank' ); the_category( ', ' ); // Separated by commas. ?></p>
-
-			<p><?php esc_html_e( 'This post was written by ', 'html5blank' ); the_author(); ?></p>
-
-			<?php edit_post_link(); // Always handy to have Edit Post Links available. ?>
-
-			<?php comments_template(); ?>
-
-		</article>
-		<!-- /article -->
-
-	<?php endwhile; ?>
-
-	<?php else : ?>
-
-		<!-- article -->
-		<article>
-
-			<h1><?php esc_html_e( 'Sorry, nothing to display.', 'html5blank' ); ?></h1>
-
-		</article>
-		<!-- /article -->
-
-	<?php endif; ?>
-
-	</section>
-	<!-- /section -->
-	</main>
-
-<?php get_sidebar(); ?>
+	<?php get_template_part( 'includes/blog' ); ?>
 
 <?php get_footer(); ?>
