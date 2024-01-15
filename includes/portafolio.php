00e8dd73b27743e255872df6a2690b8a813434b5
Luis Carlos Pando <hey@luiscarlospando.com>
2024-01-15 15:12:39 -0600

    Separar por templates

---
+++ includes/portafolio.php
@@ -0,0 +1,62 @@
+    <section id="portafolio" class="common-light py-60">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <h1>
+                        Proyectos<br>
+                        destacados
+                    </h1>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col overflow-hidden">
+                    <!-- Slider main container -->
+                    <div class="swiper-resultados">
+                        <!-- Additional required wrapper -->
+                        <div class="swiper-wrapper">
+                            <!-- Inicio Slide -->
+                            <div class="swiper-slide">
+                                <div class="row">
+                                    <div class="col-lg-6">
+                                        <h2>Farmacias Guadalajara</h2>
+                                        <a href="#">
+                                            <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/resultados/slide-1.png" alt="" class="img-fluid">
+                                        </a>
+                                    </div>
+                                    <div class="col-lg-6">
+                                        <h2>Farmacias Guadalajara</h2>
+                                        <a href="#">
+                                            <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/resultados/slide-1.png" alt="" class="img-fluid">
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- /Fin Slide -->
+                            <!-- Inicio Slide -->
+                            <div class="swiper-slide">
+                                <div class="row">
+                                    <div class="col-lg-6">
+                                        <h2>Farmacias Guadalajara</h2>
+                                        <a href="#">
+                                            <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/resultados/slide-1.png" alt="" class="img-fluid">
+                                        </a>
+                                    </div>
+                                    <div class="col-lg-6">
+                                        <h2>Farmacias Guadalajara</h2>
+                                        <a href="#">
+                                            <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/resultados/slide-1.png" alt="" class="img-fluid">
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- /Fin Slide -->
+                        </div>
+
+                        <!-- If we need navigation buttons -->
+                        <div class="swiper-button-prev"></div>
+                        <div class="swiper-button-next"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
