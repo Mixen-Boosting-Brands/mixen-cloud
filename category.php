17c6714c0bbe8c800fae3188321057621a0ce94b
Luis Carlos Pando <hey@luiscarlospando.com>
2024-01-16 11:08:37 -0600

    Armar vistas de category

--- category.php
+++ category.php
@@ -1,19 +1,55 @@
 <?php get_header(); ?>
 
-	<main role="main" aria-label="Content">
-		<!-- section -->
-		<section>
-
-			<h1><?php esc_html_e( 'Category: ', 'html5blank' ); single_cat_title(); ?></h1>
-
-			<?php get_template_part( 'loop' ); ?>
-
-			<?php get_template_part( 'pagination' ); ?>
-
-		</section>
-		<!-- /section -->
-	</main>
-
-<?php get_sidebar(); ?>
+	<section id="blog" class="common-dark py-60">
+        <div class="container">
+            <div class="row">
+                <div class="col-12my-auto">
+                    <h1>
+                        Estás en<br>
+                        <?php single_cat_title(); ?>
+                    </h1>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <div class="tab-content" id="nav-blog">
+                        <div class="tab-pane fade show active" id="nav-cat-1" role="tabpanel" aria-labelledby="nav-cat-1-tab" tabindex="0">
+                            <div class="row">
+                            <?php
+                                if (have_posts()): while (have_posts()) : the_post();
+                                    $excerpt = get_the_excerpt();
+                                    $words = explode(' ', $excerpt);
+                                    
+                                    if (count($words) > 30) {
+                                        $words = array_slice($words, 0, 30);
+                                        $excerpt = implode(' ', $words) . '...';
+                                    }
+                            ?>
+                                <div class="col-lg-6 mb-1 mb-lg-3 mb-lg-0">
+                                    <a href="<?php the_permalink(); ?>">
+                                        <?php the_post_thumbnail('home-blog', array('class' => 'img-fluid')); ?>
+                                    </a>
+                                    <a href="<?php the_permalink(); ?>">
+                                        <h2 class="mt-3">
+                                            <?php the_title(); ?>
+                                            <span class="date badge bg-secondary rounded-pill">
+                                                <time datetime="<?php the_time('Y-m-d'); ?> <?php the_time('H:i'); ?>">
+                                                    <?php echo get_the_date('M j, Y'); ?>
+                                                </time>
+                                            </span>
+                                        </h2>
+                                    </a>
+                                    <?php echo $excerpt; ?>
+                                </div>
+                            <?php 
+                                endwhile; endif;
+                            ?>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 
 <?php get_footer(); ?>
