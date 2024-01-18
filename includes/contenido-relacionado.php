c750f12fca3df67289dfd898b27b7add347ce8d7
Luis Carlos Pando <hey@luiscarlospando.com>
2024-01-18 16:12:28 -0600

    Ajustar query de contenido relacionado dependiendo del tipo de contenido que se esté visualizando

--- includes/contenido-relacionado.php
+++ includes/contenido-relacionado.php
@@ -25,17 +25,29 @@
                     }
 
                     if ( is_singular('proyectos') ) {
-                       // Query related posts based on shared tags
+                        // Query related posts based on shared taxonomy (e.g., "post_tag")
                         $args = array(
                             'post__not_in' => array(get_the_ID()), // Exclude the current post
                             'posts_per_page' => 9, // Adjust the number of related posts to display
                             'ignore_sticky_posts' => 1,
                             'post_type' => 'proyectos', // Specify the custom post type
+                            'tax_query' => array(
+                                array(
+                                    'taxonomy' => 'post_tag', // Adjust to your shared taxonomy
+                                    'terms' => wp_get_post_terms(get_the_ID(), 'post_tag', array("fields" => "ids")), // Get terms of the current post
+                                ),
+                            ),
                         );
                     } else {
-                        // Query related posts based on shared tags
+                        // Query related posts based on shared taxonomy (e.g., "post_tag")
                         $args = array(
-                            'tag__in' => $tag_ids,
+                            'tax_query' => array(
+                                array(
+                                    'taxonomy' => 'post_tag', // Adjust to your shared taxonomy
+                                    'field' => 'id',
+                                    'terms' => $tag_ids,
+                                ),
+                            ),
                             'post__not_in' => array(get_the_ID()), // Exclude the current post
                             'posts_per_page' => 9, // Adjust the number of related posts to display
                             'ignore_sticky_posts' => 1,
