ac7b0093799788950f6b1e77cbb665197f3e4af8
Luis Carlos Pando <hey@luiscarlospando.com>
2024-01-23 14:16:03 -0600

    Ajustes JP

--- index.php
+++ index.php
@@ -18,23 +18,29 @@
                             <!-- Inicio Slide -->
                             <div class="swiper-slide">
                                 <div class="row g-1">
+                                <?php if( have_rows('proyecto_1') ): while( have_rows('proyecto_1') ): the_row(); ?>
                                     <div class="col-lg-8">
-                                        <div class="thumb thumb-1">
-                                            <a href="#"></a>
+                                        <div class="thumb thumb-1" style="background: url('<?php the_sub_field('imagen'); ?>') no-repeat;">
+                                            <a href="<?php the_sub_field('link_del_proyecto'); ?>"></a>
                                         </div>
                                     </div>
+                                <?php endwhile; endif; ?>
                                     <div class="col-lg-4">
                                         <div class="row gy-1">
+                                        <?php if( have_rows('proyecto_2') ): while( have_rows('proyecto_2') ): the_row(); ?>
                                             <div class="col-12">
-                                                <div class="thumb thumb-2">
-                                                    <a href="#"></a>
+                                                <div class="thumb thumb-2" style="background: url('<?php the_sub_field('imagen'); ?>') no-repeat;">
+                                                    <a href="<?php the_sub_field('link_del_proyecto'); ?>"></a>
                                                 </div>
                                             </div>
+                                        <?php endwhile; endif; ?>
+                                        <?php if( have_rows('proyecto_3') ): while( have_rows('proyecto_3') ): the_row(); ?>
                                             <div class="col-12">
-                                                <div class="thumb thumb-3">
-                                                    <a href="#"></a>
+                                                <div class="thumb thumb-3" style="background: url('<?php the_sub_field('imagen'); ?>') no-repeat;">
+                                                    <a href="<?php the_sub_field('link_del_proyecto'); ?>"></a>
                                                 </div>
                                             </div>
+                                        <?php endwhile; endif; ?>
                                         </div>
                                     </div>
                                 </div>
