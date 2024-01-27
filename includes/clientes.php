105ab949814e8a8e50fd8419f803d890b1c21355
Luis Carlos Pando <hey@luiscarlospando.com>
2024-01-27 10:28:45 -0600

    Actualizar portafolio de clientes

--- includes/clientes.php
+++ includes/clientes.php
@@ -14,130 +14,130 @@
                         <!-- Additional required wrapper -->
                         <div class="swiper-wrapper">
                             <!-- Slides -->
-                            <div class="swiper-slide">
+                            <div class="swiper-slide my-auto">
                                 <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/clientes/Abitat.webp" alt="" class="img-fluid" loading="lazy">
                             </div>
-                            <div class="swiper-slide">
+                            <div class="swiper-slide my-auto">
                                 <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/clientes/Allstar.webp" alt="" class="img-fluid" loading="lazy">
                             </div>
-                            <div class="swiper-slide">
+                            <div class="swiper-slide my-auto">
                                 <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/clientes/American-Industries.webp" alt="" class="img-fluid" loading="lazy">
                             </div>
-                            <div class="swiper-slide">
+                            <div class="swiper-slide my-auto">
                                 <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/clientes/BCC.webp" alt="" class="img-fluid" loading="lazy">
                             </div>
-                            <div class="swiper-slide">
+                            <div class="swiper-slide my-auto">
                                 <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/clientes/Brunswick.webp" alt="" class="img-fluid" loading="lazy">
                             </div>
-                            <div class="swiper-slide">
+                            <div class="swiper-slide my-auto">
                                 <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/clientes/Burger-King.webp" alt="" class="img-fluid" loading="lazy">
                             </div>
-                            <div class="swiper-slide">
+                            <div class="swiper-slide my-auto">
                                 <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/clientes/Canadevi.webp" alt="" class="img-fluid" loading="lazy">
                             </div>
-                            <div class="swiper-slide">
+                            <div class="swiper-slide my-auto">
                                 <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/clientes/Cedehm.webp" alt="" class="img-fluid" loading="lazy">
                             </div>
-                            <div class="swiper-slide">
+                            <div class="swiper-slide my-auto">
                                 <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/clientes/Century-21.webp" alt="" class="img-fluid" loading="lazy">
                             </div>
-                            <div class="swiper-slide">
+                            <div class="swiper-slide my-auto">
                                 <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/clientes/Codetec.webp" alt="" class="img-fluid" loading="lazy">
                             </div>
-                            <div class="swiper-slide">
+                            <div class="swiper-slide my-auto">
                                 <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/clientes/Compecer.webp" alt="" class="img-fluid" loading="lazy">
                             </div>
-                            <div class="swiper-slide">
+                            <div class="swiper-slide my-auto">
                                 <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/clientes/Copamex.webp" alt="" class="img-fluid" loading="lazy">
                             </div>
-                            <div class="swiper-slide">
+                            <div class="swiper-slide my-auto">
                                 <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/clientes/Logo-CTU.webp" alt="" class="img-fluid" loading="lazy">
                             </div>
-                            <div class="swiper-slide">
+                            <div class="swiper-slide my-auto">
                                 <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/clientes/Demsa.webp" alt="" class="img-fluid" loading="lazy">
                             </div>
-                            <div class="swiper-slide">
+                            <div class="swiper-slide my-auto">
                                 <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/clientes/Dynamo.webp" alt="" class="img-fluid" loading="lazy">
                             </div>
-                            <div class="swiper-slide">
+                            <div class="swiper-slide my-auto">
                                 <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/clientes/Escala-del-Norte.webp" alt="" class="img-fluid" loading="lazy">
                             </div>
-                            <div class="swiper-slide">
+                            <div class="swiper-slide my-auto">
                                 <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/clientes/Farmacias-Guadalajara.webp" alt="" class="img-fluid" loading="lazy">
                             </div>
-                            <div class="swiper-slide">
+                            <div class="swiper-slide my-auto">
                                 <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/clientes/Fibra-Nova.webp" alt="" class="img-fluid" loading="lazy">
                             </div>
-                            <div class="swiper-slide">
+                            <div class="swiper-slide my-auto">
                                 <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/clientes/Fibras-y-Semillas.webp" alt="" class="img-fluid" loading="lazy">
                             </div>
-                            <div class="swiper-slide">
+                            <div class="swiper-slide my-auto">
                                 <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/clientes/Gobierno-del-Estado-de-Chihuahua.webp" alt="" class="img-fluid" loading="lazy">
                             </div>
-                            <div class="swiper-slide">
+                            <div class="swiper-slide my-auto">
                                 <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/clientes/Gobierno-del-Estado-de-Queretaro.webp" alt="" class="img-fluid" loading="lazy">
                             </div>
-                            <div class="swiper-slide">
+                            <div class="swiper-slide my-auto">
                                 <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/clientes/Grupo-Bafar.webp" alt="" class="img-fluid" loading="lazy">
                             </div>
-                            <div class="swiper-slide">
+                            <div class="swiper-slide my-auto">
                                 <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/clientes/Grupo-GEG.webp" alt="" class="img-fluid" loading="lazy">
                             </div>
-                            <div class="swiper-slide">
+                            <div class="swiper-slide my-auto">
                                 <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/clientes/Grupo-SIEE.webp" alt="" class="img-fluid" loading="lazy">
                             </div>
-                            <div class="swiper-slide">
+                            <div class="swiper-slide my-auto">
                                 <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/clientes/IMI.webp" alt="" class="img-fluid" loading="lazy">
                             </div>
-                            <div class="swiper-slide">
+                            <div class="swiper-slide my-auto">
                                 <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/clientes/ISAD.webp" alt="" class="img-fluid" loading="lazy">
                             </div>
-                            <div class="swiper-slide">
+                            <div class="swiper-slide my-auto">
                                 <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/clientes/ITCH.webp" alt="" class="img-fluid" loading="lazy">
                             </div>
-                            <div class="swiper-slide">
+                            <div class="swiper-slide my-auto">
                                 <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/clientes/KIA.webp" alt="" class="img-fluid" loading="lazy">
                             </div>
-                            <div class="swiper-slide">
+                            <div class="swiper-slide my-auto">
                                 <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/clientes/La-Nacional.webp" alt="" class="img-fluid" loading="lazy">
                             </div>
-                            <div class="swiper-slide">
+                            <div class="swiper-slide my-auto">
                                 <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/clientes/La-Salle.webp" alt="" class="img-fluid" loading="lazy">
                             </div>
-                            <div class="swiper-slide">
+                            <div class="swiper-slide my-auto">
                                 <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/clientes/Mercedes-Benz.webp" alt="" class="img-fluid" loading="lazy">
                             </div>
-                            <div class="swiper-slide">
+                            <div class="swiper-slide my-auto">
                                 <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/clientes/Municipio-Chihuahua.webp" alt="" class="img-fluid" loading="lazy">
                             </div>
-                            <div class="swiper-slide">
+                            <div class="swiper-slide my-auto">
                                 <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/clientes/Real.webp" alt="" class="img-fluid" loading="lazy">
                             </div>
-                            <div class="swiper-slide">
+                            <div class="swiper-slide my-auto">
                                 <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/clientes/Rigba.webp" alt="" class="img-fluid" loading="lazy">
                             </div>
-                            <div class="swiper-slide">
+                            <div class="swiper-slide my-auto">
                                 <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/clientes/RUBA.webp" alt="" class="img-fluid" loading="lazy">
                             </div>
-                            <div class="swiper-slide">
+                            <div class="swiper-slide my-auto">
                                 <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/clientes/Secretaria-de-Educacion.webp" alt="" class="img-fluid" loading="lazy">
                             </div>
-                            <div class="swiper-slide">
+                            <div class="swiper-slide my-auto">
                                 <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/clientes/Tec-MTY.webp" alt="" class="img-fluid" loading="lazy">
                             </div>
-                            <div class="swiper-slide">
+                            <div class="swiper-slide my-auto">
                                 <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/clientes/Thomson-Reuters.webp" alt="" class="img-fluid" loading="lazy">
                             </div>
-                            <div class="swiper-slide">
+                            <div class="swiper-slide my-auto">
                                 <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/clientes/Trimlight.webp" alt="" class="img-fluid" loading="lazy">
                             </div>
-                            <div class="swiper-slide">
+                            <div class="swiper-slide my-auto">
                                 <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/clientes/UACH.webp" alt="" class="img-fluid" loading="lazy">
                             </div>
-                            <div class="swiper-slide">
+                            <div class="swiper-slide my-auto">
                                 <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/clientes/Unilever.webp" alt="" class="img-fluid" loading="lazy">
                             </div>
-                            <div class="swiper-slide">
+                            <div class="swiper-slide my-auto">
                                 <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/clientes/Vitalhealth.webp" alt="" class="img-fluid" loading="lazy">
                             </div>
                         </div>
