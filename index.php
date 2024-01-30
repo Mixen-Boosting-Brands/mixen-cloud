6d2d79e8fdc727c396fb5708329962ec55a6a17f
Luis Carlos Pando <hey@luiscarlospando.com>
2024-01-30 14:38:38 -0600

    Ajustes generales

--- index.php
+++ index.php
@@ -62,7 +62,7 @@
                             </h2>
                             <div id="flush-collapseOne" class="accordion-collapse collapse show" data-bs-parent="#acordion-faq">
                                 <div class="accordion-body">
-                                    No hay nada escondido en esto, es justo lo contrario, estamos tan comprometidos en demostrar que tener presencia formal en internet es esencial para que las empresas mejoren sus prácticas y se alineen a la digitalización, que queremos y podemos ayudarlos de esta forma, es una excelente oportunidad, es algo básico informativo muy bien hecho, perfectamente funcional y estético. 😌
+                                    <p>No hay nada escondido en esto, es justo lo contrario, estamos tan comprometidos en demostrar que tener presencia formal en Internet es esencial para que las empresas mejoren sus prácticas y se alineen a la digitalización, que queremos y podemos ayudarlos de esta forma, es una excelente oportunidad, es algo básico informativo muy bien hecho, perfectamente funcional y estético. 😌</p>
                                 </div>
                             </div>
                         </div>
@@ -74,20 +74,92 @@
                             </h2>
                             <div id="flush-collapseTwo" class="accordion-collapse collapse" data-bs-parent="#acordion-faq">
                                 <div class="accordion-body">
-                                    A emprendedores, micros y pequeñas empresas que busquen formalizar su presencia en internet y no tengan disponibles los recursos económicos para invertir en un servicio personalizado de diseño y desarrollo web.
-
-                                    Universitarios que busquen tener su portafolio personalizado con dominio propio para sus proyectos escolares y prácticas profesionales, sin embargo, este servicio también lo pueden adquirir empresas medianas y grandes que necesiten tener ramas de sus proyectos internos o divisiones de negocios, ya que la infraestructura digital que ofrecemos es muy rápida, sólida y segura 👍🏽
+                                    <p>A emprendedores, micros y pequeñas empresas que busquen formalizar su presencia en Internet y no tengan disponibles los recursos económicos para invertir en un servicio personalizado de diseño y desarrollo web.</p>
+                                    <p>Universitarios que busquen tener su portafolio personalizado con dominio propio para sus proyectos escolares y prácticas profesionales, sin embargo, este servicio también lo pueden adquirir empresas medianas y grandes que necesiten tener ramas de sus proyectos internos o divisiones de negocios, ya que la infraestructura digital que ofrecemos es muy rápida, sólida y segura. 👍🏽</p>
                                 </div>
                             </div>
                         </div>
                         <div class="accordion-item">
                             <h2 class="accordion-header">
                                 <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
-                                    Accordion Item #3
+                                    ¿Es una buena relación costo beneficio?
                                 </button>
                             </h2>
                             <div id="flush-collapseThree" class="accordion-collapse collapse" data-bs-parent="#acordion-faq">
-                                <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the third item's accordion body. Nothing more exciting happening here in terms of content, but just filling up the space to make it look, at least at first glance, a bit more representative of how this would look in a real-world application.</div>
+                                <div class="accordion-body">
+                                    <p>Mixen Cloud es un "producto-servicio llave en mano", esto quiere decir que te entregamos el 100% del resultado de tu proyecto funcionando perfectamente sin que tengas que hacer esfuerzos adicionales más que pasarnos la información de tu empresa, proyecto o emprendimiento; está pensado en cubrir los requerimientos básicos para tener presencia formal en Internet, entendamos qué: sin dominio no existe tu web, ni tendrías correos, lo mismo pasa si no tienes hosting, todo lo que incluye Mixen Cloud es necesario para que tengas presencia en línea, incluso el certificado de seguridad ayuda muchísimo para una navegación segura, entonces si, si es una excelente relación de costo-beneficio; y con el regalo de tu página web, creo que es algo maravilloso que debes aprovechar ¡ya! 🤩</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="accordion-item">
+                            <h2 class="accordion-header">
+                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseFour" aria-expanded="false" aria-controls="flush-collapseFour">
+                                    Me gustaría tener más correos, solo cinco se me hacen muy pocos, ¿puedo tener más?
+                                </button>
+                            </h2>
+                            <div id="flush-collapseFour" class="accordion-collapse collapse" data-bs-parent="#acordion-faq">
+                                <div class="accordion-body">
+                                    <p>Claro, puedes tener todos los que requieras. Mixen cloud te ofrece 5 correos iniciales, si requieres más, cada cuenta de correo empresarial de 1 GB tiene un costo de $250 MXN + IVA por año. 😉</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="accordion-item">
+                            <h2 class="accordion-header">
+                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseFive" aria-expanded="false" aria-controls="flush-collapseFive">
+                                    1 GB de capacidad por correo se me hace muy poco espacio de almacenamiento, quiero más, ¿se puede?
+                                </button>
+                            </h2>
+                            <div id="flush-collapseFive" class="accordion-collapse collapse" data-bs-parent="#acordion-faq">
+                                <div class="accordion-body">
+                                    <p>Te podemos ofrecer un aumento de capacidad en tus correos, sin problema, la capacidad que sigue es de 10gb, cada cuenta de correo de 10gb tiene un costo de $750 MXN + IVA por año.</p>
+                                    <p>Es importante que te mencionemos que la unificación de capacidades en tus correos es necesaria, es decir, al momento de aumentar la capacidad de un correo, TODAS las cuentas activas de correo aumentan la capacidad de almacenamiento; de tal forma que aplica el  costo de aumento de capacidad para todas las cuentas que tengas. 🤗</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="accordion-item">
+                            <h2 class="accordion-header">
+                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseSix" aria-expanded="false" aria-controls="flush-collapseSix">
+                                    Necesito distintas capacidades de almacenamiento en mis correos, unas cuentas con 1 GB, otras con 5 GB, y unas con 10 GB, ¿se puede hacer esto?
+                                </button>
+                            </h2>
+                            <div id="flush-collapseSix" class="accordion-collapse collapse" data-bs-parent="#acordion-faq">
+                                <div class="accordion-body">
+                                    <p>Si se puede hacer, sin embargo, debemos recordar que los beneficios que ofrece Mixen Cloud tienen caracterísiticas iniciales como punto de partida, es una oferta muy concreta y clara, pero de ahí en más claro que se pueden aumentar los beneficios en general hasta donde quieras, sujeto a previa cotización, en mixen: boosting brands tenemos muchísimas soluciones en tecnología que pueden apoyarte a lograr cosas increíbles, dinos que necesitas y estamos 100% seguros que te podemos ayudar a resolverlo. 🤘</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="accordion-item">
+                            <h2 class="accordion-header">
+                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseSeven" aria-expanded="false" aria-controls="flush-collapseSeven">
+                                    No sé como dar de alta mis correos, ¿ustedes me pueden ayudar?
+                                </button>
+                            </h2>
+                            <div id="flush-collapseSeven" class="accordion-collapse collapse" data-bs-parent="#acordion-faq">
+                                <div class="accordion-body">
+                                    <p>Si podemos ayudarte y lo haremos con mucho gusto, entendemos que en ocasiones, el tiempo, las actividades o simplemente el no saber como hacerlo, representan barrerar importantes para este tipo de herramientas, nosotros nos encargamos de todo el proceso inicial, para que tú solo disfrutes del servicio. 😀</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="accordion-item">
+                            <h2 class="accordion-header">
+                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseEight" aria-expanded="false" aria-controls="flush-collapseEight">
+                                    ¿Me pueden apoyar en cualquier momento para modificar mi página web o para hacer movimientos en mis correos empresariales?
+                                </button>
+                            </h2>
+                            <div id="flush-collapseEight" class="accordion-collapse collapse" data-bs-parent="#acordion-faq">
+                                <div class="accordion-body">
+                                    <p>Si podemos ayudarte, siempre podrás contar con nosotros, solo debemos mencionarte algunas cosas importantes, completamente comunes en este giro de servicios para mantener una relación comercial sana. 🤝</p>
+                                    <ul>
+                                        <li>Únicamente atendemos soporte por medio de <a href="https://mixen.mx/soporte">ticket</a>.</li>
+                                        <li>No se reciben ni se atienden solicitudes vía WhatsApp, ni por redes sociales.</li>
+                                        <li>El horario de atención es de lunes a viernes de 9:00 am a 6:00 pm, excepto días festivos.</li>
+                                        <li>Una vez que se reciba la solicitud se analizará y se proporcionará tiempo de resolución para su autorización previa.</li>
+                                        <li>La hora o fracción de soporte (15 minutos en adelante) tiene un costo de $500 MXN más IVA.</li>
+                                        <li>Una vez realizados los ajustes, se le hará llegar la factura correspondiente al soporte.</li>
+                                        <li>Los tickets de soporte no se atienden al instante, dependen de una programación.</li>
+                                        <li>El seguimiento y/o resolución del ticket se hace únicamente vía correo electrónico.</p></li>
+                                    </ul>
+                                </div>
                             </div>
                         </div>
                     </div>
