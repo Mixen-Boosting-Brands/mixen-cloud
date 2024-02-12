d622c1cb8f8bc8515d8164e4bc8dbd8077da3852
Luis Carlos Pando <hey@luiscarlospando.com>
2024-02-12 11:19:15 -0600

    Ajustar espaciados

--- index.php
+++ index.php
@@ -87,11 +87,14 @@
                     </ul>
                 </div>
             </div>
-            <div class="row">
+            <div class="row mb-4">
                 <div class="col-lg-4 offset-lg-4 text-center">
                     <h2>¡Espera, tenemos un regalo para ti! 😎</h2>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-4 offset-lg-4 text-center">
                     <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/regalo.png" alt="¡Al adquirir Mixen Cloud te regalamos un sitio web!" class="img-fluid" id="regalo" loading="lazy">
-                    <!-- p>¡Al adquirir Mixen Cloud te regalamos un sitio web!</p -->
                 </div>
             </div>
         </div>
