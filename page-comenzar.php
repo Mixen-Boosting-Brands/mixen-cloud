3843d6e42a40b6e1f16d471974f4d54531f8fd21
Luis Carlos Pando <hey@luiscarlospando.com>
2024-02-22 18:26:52 -0600

    Desocultar página comenzar

--- page-comenzar.php
+++ page-comenzar.php
@@ -6,10 +6,12 @@
 	get_header();
 
     // Retrieve the token from the WordPress database
-    $stored_token = get_option( 'stripe_success_token' );
+    $stored_token = get_option( 'stripe_payment_token' );
+
+    // Get the token from the query parameter
+    $user_token = isset( $_GET['token'] ) ? $_GET['token'] : '';
 ?>
 
-<?php if ( isset( $_GET['token'] ) && $_GET['token'] === $stored_token ): ?>
     <section id="body" class="py-60" style="margin-top: 60px;">
         <div class="container">
             <div class="row mb-1 mb-lg-3">
@@ -28,23 +30,5 @@
             </div>
         </div>
     </section>
-<?php else: ?>
-    <section id="body" class="py-60" style="margin-top: 60px;">
-        <div class="container">
-            <div class="row mb-1 mb-lg-3">
-                <div class="col">
-                    <h1 class="titulo-page">
-                        <span class="fs-4"><?php the_title(); ?></span>
-                    </h1>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col">
-                    <p>Acceso no autorizado.</p>
-                </div>
-            </div>
-        </div>
-    </section>
-<?php endif; ?>
 
 <?php get_footer(); ?>
